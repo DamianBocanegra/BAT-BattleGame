1faf225cf783dc64892fb3dd71a70e3b63e9a920
Damian Bocanegra <d-b172@users.noreply.github.com>
2020-11-29 17:53:09 -0600

    Battle System
    Created Unit Class, BattleHUD class, BattleSystemClass;

---
+++ BattleGame/Assets/Scripts/Parents/BattleHUD.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleHUD : MonoBehaviour
+{
+    public Text nameText;
+    public Slider health;
+
+    public void setHUD(Unit unit)
+    {
+        nameText.text = unit.unitName;
+        health.maxValue = unit.maxHP;
+        health.value = unit.currentHP;
+    }
+
+    public void setHP(int hp)
+    {
+        health.value = hp;
+    }
+}
