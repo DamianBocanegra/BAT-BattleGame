d7bf362d56b15f07381f46ac499b4a2b66c30fec
Damian Bocanegra <d-b172@users.noreply.github.com>
2020-11-29 18:49:43 -0600

    Created Characters, basic funtionality achived

---
+++ BattleGame/Assets/Scripts/Enemies/Yevon.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Yevon : Unit
+{
+    private int baseDamage = 10;
+    private int ultimateDamage = 0;
+
+    //Dice rolls
+    //9 8 7 6 = charge ultimate
+    //5 4 3 = basic
+    //3 2 1 = block
+    //0 = ultimate 
+    public override string makeDesicion()
+    {
+        int diceRoll = Random.Range(0, 10);
+
+        if(diceRoll >= 6)
+        {
+            if(ultimateDamage != 0)
+            {
+                ultimateDamage = ultimateDamage * 2;
+            }
+            else
+            {
+                ultimateDamage = baseDamage;
+            }
+            dmg  = 0;
+
+            return "Yevon is gathering energy!";
+        }
+        else if(diceRoll >= 3)
+        {
+            dmg = baseDamage;
+            return "Yevon is attacking!";
+        }
+
+        else if(diceRoll >= 1)
+        {
+            blocking = true;
+            dmg = 0;
+            return "Yevon is moving into a defense position!";
+        }
+        else
+        {
+            dmg = ultimateDamage;
+            return "A unmesureable energy is being unleashed!!";
+
+        }
+        dmg = 0;
+        return "ERROR!";
+    }
+}
