7521377c86bddc8894dea84e508365a8a6408fb4
Damian Bocanegra <35676083+DamianBocanegra@users.noreply.github.com>
2020-12-09 16:25:57 -0600

    add

---
+++ BattleGame/Assets/Scripts/Parents/GameKeeper.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameKeeper : MonoBehaviour
+{
+
+    private Unit playerOne;
+
+    private Unit enemyOne;
+
+    public Unit[] players;
+    public Unit[] enemies;
+    
+    void Awake()
+    {
+        int existence = FindObjectsOfType<GameKeeper>().Length;
+        if(existence > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    
+
+    public void selectHero()
+    {
+        playerOne = players[0];
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
